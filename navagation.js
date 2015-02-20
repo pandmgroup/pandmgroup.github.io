038d9b9ad772a499009326881580cd6bed17f52e
Nat <githubpandmgroup@gmail.com>
2015-02-21 12:55:18 +1300

    initial commit of files

---
+++ navagation.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){    
+    $("#menuContact").click(function (){
+        window.location = 'contact.html';
+    });
+    $("#menuHome").click(function (){
+        window.location = 'index.html';
+    });
+    $("#menuServices").click(function (){
+        window.location = 'services.html';
+    });
+    
+    $("#menuForm").click(function (){
+        window.location = 'form.html';
+    });
+});
